837f00f239862605973ab4304eb789e851869a9a
ioanaalexandra116 <ioana.matei2108@stud.acs.pub.ro>
2023-04-25 15:13:05 +0300

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,64 @@
-import { useState } from "react";
+import React, { useState } from 'react';
+import 'firebase/auth';
+import { auth } from "./firebase-config";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut,
 } from "firebase/auth";
-import "./App.css";
-import { auth } from "./firebase-config";
-
-interface User {
-  email: string;
-}
 
 function App() {
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [loginEmail, setLoginEmail] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
 
-  const [user, setUser] = useState({});
-
-  onAuthStateChanged(auth, (currentUser) => {
-    setUser(currentUser as User);
-  });
-
-  const register = async () => {
+  const handleRegister = async () => {
     try {
-      const user = await createUserWithEmailAndPassword(
-        auth,
-        registerEmail,
-        registerPassword
-      );
-      console.log(user);
+      await createUserWithEmailAndPassword(auth, email, password);
+      setLoggedIn(true);
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   };
 
-  const login = async () => {
+  const handleLogin = async () => {
     try {
-      const user = await signInWithEmailAndPassword(
-        auth,
-        loginEmail,
-        loginPassword
-      );
-      console.log(user);
+      await signInWithEmailAndPassword(auth, email, password);
+      setLoggedIn(true);
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   };
 
-  const logout = async () => {
-    await signOut(auth);
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      setLoggedIn(false);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    <div className="App">
-      <div>
-        <h3> Register User </h3>
-        <input
-          placeholder="Email..."
-          onChange={(event) => {
-            setRegisterEmail(event.target.value);
-          }}
-        />
-        <input
-          placeholder="Password..."
-          onChange={(event) => {
-            setRegisterPassword(event.target.value);
-          }}
-        />
-
-        <button onClick={register}> Create User</button>
-      </div>
-
-      <div>
-        <h3> Login </h3>
-        <input
-          placeholder="Email..."
-          onChange={(event) => {
-            setLoginEmail(event.target.value);
-          }}
-        />
-        <input
-          placeholder="Password..."
-          onChange={(event) => {
-            setLoginPassword(event.target.value);
-          }}
-        />
-
-        <button onClick={login}> Login</button>
-      </div>
-
-      <h4> User Logged In: </h4>
-      {/* {user?.email} but in typescript */}
-      {user && (user as User).email}
-
-      <button onClick={logout}> Sign Out </button>
+    <div>
+      {loggedIn ? (
+        <div>
+          <h1>You are logged in!</h1>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <label>
+            Email:
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </label>
+          <label>
+            Password:
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </label>
+          <button onClick={handleRegister}>Register</button>
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      )}
     </div>
   );
 }
