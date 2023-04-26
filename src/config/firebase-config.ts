7e2b14782fff9facca62740d2198e9693a5a6ad5
Ioana-Alexandra Matei <74566718+ioanaalexandra116@users.noreply.github.com>
2023-04-26 22:12:15 +0300

    users collection (#5)
    Co-authored-by: ioanaalexandra116 <ioana.matei2108@stud.acs.pub.ro>

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: 'AIzaSyCMkF-jIdQ3egqehoZUXqePsmZdK7Nfobw',
@@ -12,3 +13,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+
