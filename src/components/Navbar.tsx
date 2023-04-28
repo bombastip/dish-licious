c15e26a208ec09ce937ee0631370e354da671386
Ioana-Alexandra <ioanaalexandra116@gmail.com>
2023-04-28 08:50:24 +0300

    turned friends in followers and following

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -122,8 +122,11 @@ function NavbarF() {
                 <Dropdown.Item key="profile" withDivider>
                   My Profile
                 </Dropdown.Item>
-                <Dropdown.Item key="friends" withDivider>
-                  Friends
+                <Dropdown.Item key="followers" withDivider>
+                  Followers
+                </Dropdown.Item>
+                <Dropdown.Item key="following" withDivider>
+                  Following
                 </Dropdown.Item>
                 <Dropdown.Item key="groups" withDivider>
                   Groups
