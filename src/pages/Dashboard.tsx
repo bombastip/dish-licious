391b265181b5154b02f4b8c9df61d7a7e3e74503
Silviu <silviu.florian@stud.acs.upb.ro>
2023-05-12 18:03:57 +0300

    solved type error

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
-import { Post } from '../components/Design-post';
+// import { Post } from '../components/Design-post';
 
 const Dashboard = () => {
-    return <div>{/* <Post></Post> */}</div>;
+    return <>{/* <Post></Post> */}</>;
 };
 
 export default Dashboard;
