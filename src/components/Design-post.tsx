7a4813de4c657f7a20875d0db8c928fbd14a927c
alexandra.barbu <abarbu302@gmail.com>
2023-05-13 22:26:11 +0300

    feed fixed

--- src/components/Design-post.tsx
+++ src/components/Design-post.tsx
@@ -1,33 +1,41 @@
 import { Grid, Spacer } from '@nextui-org/react';
 import SinglePost from './SinglePost';
 import { useEffect, useState } from 'react';
-import { getDocs, collection } from 'firebase/firestore';
+import { getDocs, getDoc, collection, doc, query, where } from 'firebase/firestore';
 import { db } from '../config/firebase-config';
 import { PostType } from '../interfaces';
+import { useContext } from 'react';
+import { AuthContext } from '../context';
 
 export const Post = () => {
     const [postList, setPostList] = useState<PostType[]>([
         { userID: '', title: '', description: '', photoURL: '', likes: [], timeCost: 0, timeUnit: '', id: '' },
     ]);
     const postCollectionRef = collection(db, 'posts');
+    const { user } = useContext(AuthContext);
 
     useEffect(() => {
-        const getPostList = async () => {
-            // READ THE DATA
-            // SET THE POST LIST
-            try {
-                const data = await getDocs(postCollectionRef);
-                const filteredData = data.docs.map(doc => ({
+        if (user) {
+            const getPostList = async () => {
+                // READ THE DATA
+                // SET THE POST LIST
+                const userdocRef = doc(db, 'users', user.uid);
+                const docUserSnap = await getDoc(userdocRef);
+                const following = docUserSnap.data()?.following;
+                // setFollowing(following);
+
+                // Filter posts by following users
+                const q = query(postCollectionRef, where('userID', 'in', following));
+                const querySnapshot = await getDocs(q);
+                const filteredData = querySnapshot.docs.map(doc => ({
                     ...(doc.data() as PostType),
                     id: doc.id,
                 }));
                 setPostList(filteredData);
-            } catch (err) {
-                console.error(err);
-            }
-        };
-        getPostList();
-    }, []);
+            };
+            getPostList();
+        }
+    }, [user]);
 
     return (
         <Grid.Container gap={2} justify="center" css={{ marginTop: '20px' }}>
