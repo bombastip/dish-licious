6b31a883d6ceee14d4ae450be4462bbb7f6057a4
Ioana-Alexandra <ioanaalexandra116@gmail.com>
2023-05-24 16:50:34 +0300

    list of groups

--- src/components/Groups/GroupList.tsx
+++ src/components/Groups/GroupList.tsx
@@ -1,14 +1,44 @@
-import { Container } from '@nextui-org/react';
+import { Container, Loading } from '@nextui-org/react';
 import { GroupCard } from '.';
-// import { GroupCardInfo } from '../../interfaces';
+import { GroupType } from '../../interfaces';
+import { AuthContext } from '../../context';
+import { useContext, useEffect, useState } from 'react';
+import { getGroups, getGroupData } from '../../database';
 
 const GroupList = () => {
-    // luat grupurile din baza de date si afisate cu GroupCards
-    const groupCardInfo = ['grup1', 'grup2', 'grup3', 'grup4', 'grup5', 'grup6', 'grup7', 'grup8', 'grup9', 'grup10'];
+    const { user } = useContext(AuthContext);
+    const [groupIds, setGroupIds] = useState<string[]>([]);
+    const [groupCardInfo, setGroupCardInfo] = useState<GroupType[]>([]);
+    if (!user) {
+        return <Loading />;
+    }
+    useEffect(() => {
+        const getGroupsList = async () => {
+            const groups = await getGroups(user.uid);
+            console.log('groups: ', groups);
+            setGroupIds(groups);
+        };
+        getGroupsList();
+    }, [user]);
+
+    useEffect(() => {
+        const getGroupsData = async () => {
+            groupIds.map(async groupId => {
+                const groupData = await getGroupData(groupId);
+                if (!groupData) {
+                    return;
+                }
+                groupData.id = groupId;
+                setGroupCardInfo(groupCardInfo => [...groupCardInfo, groupData] as GroupType[]);
+            }, []);
+        };
+        getGroupsData();
+    }, [groupIds]);
+
     return (
         <Container css={{ display: 'flex', justifyContent: 'center' }}>
             {groupCardInfo.map(group => (
-                <GroupCard groupCardInfo={group} key={group} />
+                <GroupCard groupCardInfo={group} key={group.name} />
             ))}
         </Container>
     );
